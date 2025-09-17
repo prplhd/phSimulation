0c3705e23aaa981717ea32770691d0ada0b04a58
prplhd <dimas.kovalenko2000@mail.ru>
2025-09-18 00:38:52 +0700

    change name from SimulationPreset.java to SimulationFactory.java

---
+++ src/main/java/phsimulation/config/SimulationFactory.java
@@ -0,0 +1,114 @@
+package main.java.phsimulation.config;
+
+import main.java.phsimulation.exceptions.UnknownWorldSizeException;
+import main.java.phsimulation.pathfinding.BreadthFirstSearchPathfinder;
+
+public final class SimulationFactory {
+    public static final int SMALL_WORLD_KEY = 1;
+    public static final int MEDIUM_WORLD_KEY = 2;
+    public static final int LARGE_WORLD_KEY = 3;
+
+    private SimulationFactory(){}
+
+    public static SimulationConfig getForSize(int size) {
+        return switch (size){
+            case SMALL_WORLD_KEY -> createSmallSimulationConfig();
+            case MEDIUM_WORLD_KEY -> createMediumSimulationConfig();
+            case LARGE_WORLD_KEY -> createLargeSimulationConfig();
+            default -> throw new UnknownWorldSizeException(size);
+        };
+    }
+
+    private static SimulationConfig createSmallSimulationConfig() {
+        return new SimulationConfig.Builder()
+                .worldMap()
+                .height(10)
+                .width(10)
+                .done()
+                .predator()
+                .speed(2)
+                .maxHp(100)
+                .attackPower(22)
+                .done()
+                .herbivore()
+                .speed(1)
+                .maxHp(60)
+                .hpRestoredPerGrass(20)
+                .done()
+                .entitiesCount()
+                .predatorCount(1)
+                .herbivoreCount(3)
+                .grassCount(10)
+                .treeCount(6)
+                .rockCount(6)
+                .done()
+                .minCounts()
+                .grassMinCount(3)
+                .herbivoreMinCount(1)
+                .done()
+                .pathfinder(new BreadthFirstSearchPathfinder())
+                .build();
+    }
+
+    private static SimulationConfig createMediumSimulationConfig() {
+        return new SimulationConfig.Builder()
+                .worldMap()
+                .height(15)
+                .width(15)
+                .done()
+                .predator()
+                .speed(2)
+                .maxHp(100)
+                .attackPower(25)
+                .done()
+                .herbivore()
+                .speed(1)
+                .maxHp(60)
+                .hpRestoredPerGrass(10)
+                .done()
+                .entitiesCount()
+                .predatorCount(2)
+                .herbivoreCount(6)
+                .grassCount(20)
+                .treeCount(12)
+                .rockCount(12)
+                .done()
+                .minCounts()
+                .grassMinCount(6)
+                .herbivoreMinCount(2)
+                .done()
+                .pathfinder(new BreadthFirstSearchPathfinder())
+                .build();
+    }
+
+    private static SimulationConfig createLargeSimulationConfig() {
+        return new SimulationConfig.Builder()
+                .worldMap()
+                .height(18)
+                .width(18)
+                .done()
+                .predator()
+                .speed(2)
+                .maxHp(100)
+                .attackPower(25)
+                .done()
+                .herbivore()
+                .speed(1)
+                .maxHp(60)
+                .hpRestoredPerGrass(10)
+                .done()
+                .entitiesCount()
+                .predatorCount(3)
+                .herbivoreCount(9)
+                .grassCount(30)
+                .treeCount(18)
+                .rockCount(18)
+                .done()
+                .minCounts()
+                .grassMinCount(9)
+                .herbivoreMinCount(3)
+                .done()
+                .pathfinder(new BreadthFirstSearchPathfinder())
+                .build();
+    }
+}
--- src/main/java/phsimulation/messages/DialogMessages.java
+++ src/main/java/phsimulation/messages/DialogMessages.java
@@ -1,6 +1,6 @@
 package main.java.phsimulation.messages;
 
-import main.java.phsimulation.config.SimulationPreset;
+import main.java.phsimulation.config.SimulationFactory;
 
 public final class DialogMessages {
     public static final String PRESET_SELECT_DIALOG_TITLE = """
@@ -8,7 +8,7 @@ public final class DialogMessages {
             %d. Маленький
             %d. Средний
             %d. Большой
-            """.formatted(SimulationPreset.SMALL_WORLD_KEY, SimulationPreset.MEDIUM_WORLD_KEY, SimulationPreset.LARGE_WORLD_KEY);
+            """.formatted(SimulationFactory.SMALL_WORLD_KEY, SimulationFactory.MEDIUM_WORLD_KEY, SimulationFactory.LARGE_WORLD_KEY);
 
     public static final String PRESET_SELECT_DIALOG_ERROR = "Неверный ввод. Попробуйте еще раз \n";
 
